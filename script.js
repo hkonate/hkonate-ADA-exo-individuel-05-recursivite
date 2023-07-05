b14d8a024026823ea5fd996f54fbd55cd9bd59f8
hkonate <konate.tronca@gmail.com>
2023-07-05 23:10:22 +0200

    step 3 done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const sum1 = (tab) => {
   return result;
 };
 
-// console.log(sum1([1, 5, 8, 9, 10])); 33
+// console.log(sum1([1, 5, 8, 9, 10])); // return 33
 
 //Step2
 
@@ -14,4 +14,14 @@ const sum2 = (tab) => {
   if (tab.length === 1) return tab[0];
   return tab.pop() + sum2(tab);
 };
-console.log(sum2([1, 5, 8, 9, 15]));
+
+// console.log(sum2([1, 5, 8, 9, 15])); // return 38
+
+//Step 3
+
+const factorial = (int) => {
+  if (int === 1) return 1;
+  return int * factorial(int - 1);
+};
+
+//console.log(factorial(5)); // return 120
