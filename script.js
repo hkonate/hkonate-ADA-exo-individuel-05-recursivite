c9e2144580d3a8b6f3270aa5d96a3922c6cad8e1
hkonate <konate.tronca@gmail.com>
2023-07-05 23:00:54 +0200

    step 2 done

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const sum1 = (tab) => {
 };
 
 // console.log(sum1([1, 5, 8, 9, 10])); 33
+
+//Step2
+
+const sum2 = (tab) => {
+  if (tab.length === 1) return tab[0];
+  return tab.pop() + sum2(tab);
+};
+console.log(sum2([1, 5, 8, 9, 15]));
